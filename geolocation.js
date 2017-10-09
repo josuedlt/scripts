80ce77391aed220c846147109ec00f7ad16f052b
Josue de la Torre <josue@jdlt.com>
2017-10-09 00:10:07 -0500

    Error cases

--- geolocation.js
+++ geolocation.js
@@ -19,8 +19,21 @@ var Geolocation = function () {
     }
   };
 
-  function error(e) {
-    console.log(e);
+  function error(error) {
+    switch (error.code) {
+      case error.PERMISSION_DENIED:
+        console.log("User denied the request for Geolocation.");
+        break;
+      case error.POSITION_UNAVAILABLE:
+        console.log("Location information is unavailable.");
+        break;
+      case error.TIMEOUT:
+        console.log("The request to get user location timed out.");
+        break;
+      case error.UNKNOWN_ERROR:
+        console.log("An unknown error occurred.");
+        break;
+    }
   };
 
   return {
