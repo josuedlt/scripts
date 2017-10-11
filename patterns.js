70a013553c02c8ca1da143104731a44207b093a6
Josue de la Torre <josue@jdlt.com>
2017-10-10 22:27:47 -0500

    Updated patterns, added KeyedSubscriber

--- patterns.js
+++ patterns.js
@@ -1,42 +1,59 @@
-var Subscriber = function() {
-    var handlers = [];
-  
-    return {
-      fire: function (o, thisObj) {
-        var scope = thisObj || window;
-        handlers.forEach(function (fn) {
-          fn.call(scope, o);
-        });
-      },
-      subscribe: function (fn) {
-        handlers.push(fn);
-      },
+var KeyedSubscriber = function () {
+  var handlers = {};
 
-      unsubscribe: function (fn) {
-        handlers = handlers.filter(
-          function (item) {
-            if (item !== fn) {
-              return item;
-            }
+  return {
+    fire: function (key, o) {
+      if (handlers[key])
+        handlers[key].forEach(function (fn) { fn(o) });
+    },
+    on: function (key, fn) {
+      if (!handlers[key])
+        handlers[key] = new Array;
+
+      handlers[key].push(fn);
+    }
+  }
+}
+
+var Subscriber = function () {
+  var handlers = [];
+
+  return {
+    fire: function (o, thisObj) {
+      var scope = thisObj || window;
+      handlers.forEach(function (fn) {
+        fn.call(scope, o);
+      });
+    },
+    subscribe: function (fn) {
+      handlers.push(fn);
+    },
+
+    unsubscribe: function (fn) {
+      handlers = handlers.filter(
+        function (item) {
+          if (item !== fn) {
+            return item;
           }
-        );
-      }
-    }
-  };
-  
-  var Singleton = (function () {
-    var instance;
-  
-    function createInstance(fn) {
-      return fn;
-    }
-  
-    return {
-      getInstance: function (fn) {
-        if (!instance) {
-          instance = createInstance(fn);
         }
-        return instance;
-      }
+      );
     }
-  })();
+  }
+};
+
+var Singleton = (function () {
+  var instance;
+
+  function createInstance(fn) {
+    return fn;
+  }
+
+  return {
+    getInstance: function (fn) {
+      if (!instance) {
+        instance = createInstance(fn);
+      }
+      return instance;
+    }
+  }
+})();
