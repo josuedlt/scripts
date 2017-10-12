01b867ee7668afb4b4b544527a902ddbe136774a
Josue de la Torre <josue@jdlt.com>
2017-10-11 22:29:52 -0500

    Added signalr path

---
+++ signalr/ws.signalR.js
@@ -0,0 +1,67 @@
+var signalR = function (hubPath) {
+    var _this = this;
+
+    // Configure subscribers...
+    _this.keyHandlers = {};
+    _this.fire = function (key, o) {
+        if (_this.keyHandlers[key])
+            _this.keyHandlers[key].forEach(function (fn) {
+                fn(o)
+            });
+    }
+
+    // Configure connection
+    _this.prepareConnection = function () {
+        _this.connection = $.hubConnection(hubPath);
+        _this.connection.stateChanged(function (change) {
+            function stateName(state) {
+                switch (state) {
+                    case ($.signalR.connectionState.connecting):
+                        return "connecting";
+                    case ($.signalR.connectionState.connected):
+                        return "connected";
+                    case ($.signalR.connectionState.disconnected):
+                        return "disconnected";
+                    case ($.signalR.connectionState.reconnecting):
+                        return "reconnecting";
+                }
+            }
+            _this.fire('state', stateName(change.newState));
+        })
+
+        _this.connectToHub = function () {
+            _this.connection.start().done(function () {
+                _this.fire('ready', _this.connection.createHubProxies());
+
+                window.onbeforeunload = function () {
+                    _this.connection.stop();
+                };
+                _this.connection.disconnected(function () {
+                    setTimeout(function () {
+                        _this.connectToHub();
+                    }, 5000);
+                });
+            })
+
+        }
+        
+        _this.connectToHub();
+    }
+
+    $.ajax({
+        async: false,
+        dataType:'script',
+        url: hubPath + '/signalr/hubs',
+        success: _this.prepareConnection
+    });
+
+    return {
+        on: function (key, fn) {
+            if (!_this.keyHandlers[key])
+                _this.keyHandlers[key] = new Array;
+
+            _this.keyHandlers[key].push(fn);
+            return this;
+        }
+    }
+};
