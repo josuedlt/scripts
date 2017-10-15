76cdd196e11647d0c78f702a58ee9fc5f693b1fb
Josue de la Torre <josue@jdlt.com>
2017-10-15 18:51:46 -0500

    Updates to signalr

--- signalr/jdlt.signalRHubs.js
+++ signalr/jdlt.signalRHubs.js
@@ -1,32 +1,34 @@
-function signalRHub(url, callback) {
-    $.ajax({
-        async: false,
-        cache: false,
-        dataType: 'script',
-        url: url + '/signalr/hubs',
-        complete: function() {
-            h = $.hubConnection(url)
-            h.createHubProxies();
-            callback(h);
-        }
-    })
-}
-
-var signalRHubs = function (urls, callback) {
-    var hubs = [];
-    urls.forEach(function (url) {
+signalR = function () {};
+signalR.prototype = {
+    connectToHub: function(url, callback) {
         $.ajax({
             async: false,
             cache: false,
             dataType: 'script',
             url: url + '/signalr/hubs',
             complete: function () {
-                h = $.hubConnection(url);
+                h = $.hubConnection(url)
                 h.createHubProxies();
-                hubs.push(h);
-                if (hubs.length == urls.length) 
-                    callback(hubs);
+                callback(h);
             }
-        })
-    })
-}
+        });
+    },
+    connectToHubs: function(urls, callback) {
+        var hubs = [];
+        urls.forEach(function (url) {
+            $.ajax({
+                async: false,
+                cache: false,
+                dataType: 'script',
+                url: url + '/signalr/hubs',
+                complete: function () {
+                    h = $.hubConnection(url);
+                    h.createHubProxies();
+                    hubs.push(h);
+                    if (hubs.length == urls.length)
+                        callback(hubs);
+                }
+            });
+        });
+    }
+};
