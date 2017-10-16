7e861912161dfa76aca2284520d40ecc1a21f1fb
Josue de la Torre <josue@jdlt.com>
2017-10-15 23:42:11 -0500

    cleaned up singalRHubs

--- signalr/jdlt.signalRHubs.js
+++ signalr/jdlt.signalRHubs.js
@@ -1,27 +1,19 @@
-signalR = function () {};
+var signalR = function () {};
 signalR.prototype = {
-    connectToHub: function(url, callback) {
-        $.ajax({
-            async: false,
-            cache: false,
-            dataType: 'script',
-            url: url + '/signalr/hubs',
-            complete: function () {
-                h = $.hubConnection(url)
-                h.createHubProxies();
-                callback(h);
-            }
+    connectToHub: function (url, callback) {
+        this.connectToHubs([url], (hubs) => {
+            callback(hubs[0]);
         });
     },
-    connectToHubs: function(urls, callback) {
+    connectToHubs: (urls, callback) => {
         var hubs = [];
-        urls.forEach(function (url) {
+        urls.forEach((url) => {
             $.ajax({
                 async: false,
                 cache: false,
                 dataType: 'script',
                 url: url + '/signalr/hubs',
-                complete: function () {
+                complete: () => {
                     h = $.hubConnection(url);
                     h.createHubProxies();
                     hubs.push(h);
@@ -30,5 +22,65 @@ signalR.prototype = {
                 }
             });
         });
+    },
+    logConnectionStates: function (hub) {
+        _this = this;
+
+        hub.connectionSlow(() => {
+            if (_this.logging) console.log('%s connection slow');
+        });
+
+        hub.stateChanged((change) => {
+            // Get connection state name.
+            stateName = (state) => {
+                switch (state) {
+                    case ($.signalR.connectionState.connecting):
+                        return "connecting";
+                    case ($.signalR.connectionState.connected):
+                        return "connected";
+                    case ($.signalR.connectionState.disconnected):
+                        return "disconnected";
+                    case ($.signalR.connectionState.reconnecting):
+                        return "reconnecting";
+                }
+            }
+            if (_this.logging) console.log('%s state changed from %s to %s', hub.url, stateName(change.oldState),
+                stateName(change.newState));
+        });
+
+        return {
+            start: function () {
+                _this.logging = true;
+            },
+            stop: function () {
+                _this.logging = false;
+            }
+        }
+
+    },
+    persistedConnection: function (hub) {
+        _this = this;
+
+        return {
+            start: function () {
+                _this.tryingToReconnect = true;
+                hub.start().done(() => {
+                    hub.disconnected(() => {
+                        setTimeout(() => {
+                            if (_this.tryingToReconnect)
+                                _this.startPersistedConnection(hub);
+                        }, 5000);
+                    });
+                });
+                return this;
+            },
+            stop: function () {
+                _this.tryingToReconnect = false;
+                hub.stop();
+                return this;
+            }
+        }
     }
 };
+
+var signalR = new signalR();
