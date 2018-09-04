e60edd12bc949f7f65cbb98646add8a81f9aadad
Josue de la Torre <josue@jdlt.com>
2018-09-04 01:34:37 -0500

    .

--- integration/angular-nisdArmoredService.js
+++ integration/angular-nisdArmoredService.js
@@ -1,17 +1,85 @@
 var app = angular.module('nisdArmoredService', [])
     .factory('ArmoredService', ($http) => {
-        var baseUrl = 'https://integration.nisd.net/api/';
+        var baseUrl = 'https://integration.nisd.net';
+
+        function SignalR() {
+            var handlers = {};
+
+            function fire(key, o) {
+                if (handlers[key])
+                    handlers[key].forEach(function (fn) { fn(o); });
+            }
+
+            signalR.connectToHub(baseUrl)
+                .then((hub) => {
+                    proxy = hub.proxies.armoredservicehub;
+                    proxy.client = {
+                        refresh() {
+                            fire("refresh");
+                        }
+                    };
+                    hub.start();
+                });
+
+            // Connect to webservices to get connection updates
+            signalR.connectToHub('https://webservices.nisd.net')
+                .then((hub) => {
+                    var connections = [];
+                    var proxy = hub.proxies.connectionshub;
+                    proxy.client = {
+                        hasConnected: function (c) {
+                            if (c.ConnectionId == hub.id) {
+                                _id = hub.id;
+                                proxy.server.getConnections().done(function (connections) {
+                                    _connections = connections;
+                                    fire('connections', _connections);
+                                });
+                            } else {
+                                var i = _.indexOf(_.map(_connections, 'ConnectionId'), c.ConnectionId)
+                                if (i > -1) _connections[i] = c;
+                                else _connections.push(c);
+                                fire('connections', _connections);
+                            }
+                        },
+                        hasDisconnected: function (c) {
+                            if (c.ConnectionId == hub.id) {
+
+                            } else {
+                                var i = _.indexOf(_.map(_connections, 'ConnectionId'), c.ConnectionId);
+                                if (i > -1) _connections.splice(i, 1);
+                            }
+
+                            fire('connections', _connections);
+                        },
+                        hasUpdatedPosition: function (c, p) {
+                            var i = _.indexOf(_.map(_connections, 'ConnectionId'), c.ConnectionId)
+                            if (i > -1) _connections[i].Position = p;
+
+                            fire('connections', _connections);
+                        }
+                    }
+                    hub.start();
+                })
+
+            return {
+                on: function (key, fn) {
+                    if (!handlers[key]) handlers[key] = [];
+                    handlers[key].push(fn);
+                }
+            };
+        }
 
         return {
             // API Calls 
             GetLocations: function () {
-                return $http.get(baseUrl + 'ArmoredService/GetLocations');
+                return $http.get(baseUrl + '/api/ArmoredService/GetLocations');
             },
             GetSubmittedDepositBags: function () {
-                return $http.get(baseUrl + 'ArmoredService/GetPendingDepositBags');
+                return $http.get(baseUrl + '/api/ArmoredService/GetPendingDepositBags');
             },
             GetCollectedDepositBags: function () {
-                return $http.get(baseUrl + 'ArmoredService/GetCollectedDepositBags');
+                return $http.get(baseUrl + '/api/ArmoredService/GetCollectedDepositBags');
             },
+            SignalR: SignalR(),
         }
     })
--- integration/angular-nisdMapbox.js
+++ integration/angular-nisdMapbox.js
@@ -8,32 +8,7 @@ var app = angular.module('nisdMapbox', [])
     .directive('mapbox', () => {
         return {
             link: (scope, elem, attrs) => {
-                mapboxgl.accessToken =
-                    'pk.eyJ1Ijoiam9zdWVkbHQiLCJhIjoiY2l5a3hwcGFzMDAxdjJ4czd0dW1nNnpvbiJ9.118F1NOgT72aFsmG99qPYQ';
-
-                var mapbox = scope.mapbox = new mapboxgl.Map({
-                    container: elem[0],
-                    style: 'mapbox://styles/mapbox/streets-v10/',
-                    center: [-98.5, 29.43],
-                    zoom: 10
-                });
-
-                // Script to map sources
-                mapbox.mapSources = (sources) => {
-                    sources.forEach((s) => {
-                        if (!mapbox.getSource(s.id))
-                            mapbox.addSource(s.id, { 'type': 'geojson', 'data': s.data });
-                        else
-                            mapbox.getSource(s.id).setData(s.data);
-                    })
-                }
-
-                mapbox.mapLayers = (layers) => {
-                    layers.forEach((l) => {
-                        if (!mapbox.getLayer(l.id))
-                            mapbox.addLayer(l);
-                    })
-                }
+                
             }
         }
     })
